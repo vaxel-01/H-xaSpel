e1270fa459fe2bf525904167673c63a3d7de6d8d
BackeTako <branne33@gmail.com>
2024-07-25 13:35:46 +0200

    Gunter Can die now

--- Assets/Scripts/ChildCollect.cs
+++ Assets/Scripts/ChildCollect.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class ChildCollect : MonoBehaviour
 {
+    public Animator animator;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
+            animator.SetTrigger("TakeChild");
+            
             PlayerMovement player = collision.gameObject.GetComponent<PlayerMovement>();
             player.CollectChild();
-
-            Destroy(gameObject);
         }
     }
 }
